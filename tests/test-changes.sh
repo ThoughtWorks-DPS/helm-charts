4a72a159fd16098a82d73b4d36ba0e78e8ce5314
Nic Cheneweth <nchenewe@thoughtworks.com>
2023-01-10 19:49:18 -0600

    [nc] change testing method
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- tests/test-changes.sh
+++ tests/test-changes.sh
@@ -1,21 +1,19 @@
-#!/usr/bin/env bash
+
 
 # find all changed charts in the triggering commit
 ct list-changed > charts-to-test
 
-while read chartpath; do
-  chart=${chartpath##*/}
-  echo ${#chart}
-  echo "testing $chart"
-  cd tests
-  cd $chart
-  pwd
-  cd ../..
-  pwd
-  # go mod init "github.com/ThoughtWorks-DPS/helm-charts"
-  # go mod tidy
-  # go test -v -timeout 20m
-  # cd ../..
-done <charts-to-test
-
-#opa-sidecar-admission-controller
+# while read chartpath; do
+#   chart=${chartpath##*/}
+#   echo ${#chart}
+#   echo "testing $chart"
+#   cd tests
+#   cd $chart
+#   pwd
+#   cd ../..
+#   pwd
+#   # go mod init "github.com/ThoughtWorks-DPS/helm-charts"
+#   # go mod tidy
+#   # go test -v -timeout 20m
+#   # cd ../..
+# done <charts-to-test
